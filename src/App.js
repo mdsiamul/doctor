7d67330083482571e722e5d1d23199abcbdf2480
mdsiamul <mdsiamulsuratprince@gmail.com>
2022-08-07 01:27:36 +0600

    card

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Pages/Shared/Navbar';
 import { Routes, Route } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 
 function App() {
@@ -12,7 +13,9 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
---
+++ src/Pages/Home/Info.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import clockicon from '../../assets/icons/clock.svg';
+import visiticon from '../../assets/icons/marker.svg';
+import contacticon from '../../assets/icons/phone.svg';
+
+const Info = () => {
+    return (
+        <div class='flex m-0 grid grid-cols-1 lg:grid-cols-3 items-center'>
+            <div class="card w-96 bg-primary  m-5  grid grid-cols-2 gap-0 flex  items-center p-3">
+                <div>
+                    <img src={clockicon} alt="" />
+                </div>
+                <div class="card-body p-0 items-center ">
+                    <h2 class="card-title">Opening Hours</h2>
+                    <p>We are using cookies for no reason.</p>
+
+                </div>
+            </div>
+            <div class="card w-96 bg-neutral text-neutral-content m-5  grid grid-cols-2 gap-0 flex  items-center p-3">
+                <div>
+                    <img src={visiticon} alt="" />
+                </div>
+                <div class="card-body p-0 items-center ">
+                    <h2 class="card-title">Opening Hours</h2>
+                    <p>We are using cookies for no reason.</p>
+
+                </div>
+            </div>
+            <div class="card w-96 bg-primary  m-5  grid grid-cols-2 gap-0 flex  items-center p-3">
+                <div>
+                    <img src={contacticon} alt="" />
+                </div>
+                <div class="card-body p-0 items-center ">
+                    <h2 class="card-title">Opening Hours</h2>
+                    <p>We are using cookies for no reason.</p>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Info;
