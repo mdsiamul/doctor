3095c6355299e38d3652133ad60e9deddd682f9b
mdsiamul <mdsiamulsuratprince@gmail.com>
2022-08-30 15:04:05 +0600

    f

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -5,33 +5,33 @@ import contacticon from '../../assets/icons/phone.svg';
 
 const Info = () => {
     return (
-        <div class='flex m-0 grid grid-cols-1 lg:grid-cols-3 gap-1 items-center'>
-            <div class="card bg-primary  m-5  grid grid-cols-2 gap-1 flex  items-center p-3">
+        <div className='flex m-0 grid grid-cols-1 lg:grid-cols-3 gap-1 items-center'>
+            <div className="card bg-primary  m-5  grid grid-cols-2 gap-1 flex  items-center p-3">
                 <div>
                     <img src={clockicon} alt="" />
                 </div>
-                <div class="card-body p-0 items-center ">
-                    <h2 class="card-title">Opening Hours</h2>
+                <div className="card-body p-0 items-center ">
+                    <h2 className="card-title">Opening Hours</h2>
                     <p>We are using cookies for no reason.</p>
 
                 </div>
             </div>
-            <div class="card  bg-neutral text-neutral-content m-5  grid grid-cols-2 gap-0 flex  items-center p-3">
+            <div className="card  bg-neutral text-neutral-content m-5  grid grid-cols-2 gap-0 flex  items-center p-3">
                 <div>
                     <img src={visiticon} alt="" />
                 </div>
-                <div class="card-body p-0 items-center ">
-                    <h2 class="card-title">Hours</h2>
+                <div className="card-body p-0 items-center ">
+                    <h2 className="card-title">Hours</h2>
                     <p>We are using cookies for no reason.</p>
 
                 </div>
             </div>
-            <div class="card  bg-primary  m-5  grid grid-cols-2 gap-0 flex  items-center p-3">
+            <div className="card  bg-primary  m-5  grid grid-cols-2 gap-0 flex  items-center p-3">
                 <div>
                     <img src={contacticon} alt="" />
                 </div>
-                <div class="card-body p-0 items-center ">
-                    <h2 class="card-title">Opening Hours</h2>
+                <div className="card-body p-0 items-center ">
+                    <h2 className="card-title">Opening Hours</h2>
                     <p>We are using cookies for no reason.</p>
 
                 </div>
--- src/Pages/Home/Makeappoint.js
+++ src/Pages/Home/Makeappoint.js
@@ -16,7 +16,7 @@ const Makeappoint = () => {
                 <h3 className='text-xl text-primary'>Appointment</h3>
                 <h2 className='text-3xl text-white m-5'>Make and Appointment Today</h2>
                 <p className='text-white m-5'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
-                <button class="btn btn-primary">Get Started</button>
+                <button className="btn btn-primary">Get Started</button>
             </div>
         </section>
 
